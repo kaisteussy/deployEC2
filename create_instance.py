687be23fbeeefcda082d71ec38f77216ef1d5eec
ksteussy <kai.steussy@gmail.com>
2021-05-06 21:32:10 -0500

    Replaced static list of volumes with a for loop for flexibility in config.yml

--- create_instance.py
+++ create_instance.py
@@ -1,11 +1,42 @@
 import boto3
+import yaml
+
+volumes = []
+
+# Set the resource object
 ec2 = boto3.resource('ec2')
 
-# create a new EC2 instance
-instances = ec2.create_instances(
-     ImageId='ami-00b6a8a2bd28daf19',
-     MinCount=1,
-     MaxCount=2,
-     InstanceType='t2.micro',
-     KeyName='ec2-keypair'
- )
+# Open the configuration file and store its contents
+with open('config.yml', 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+# Iterate through and store the volumes listed in config.yml
+for volume in config['server']['volumes']:
+    volumes.append({'DeviceName': volume['device'],
+                    'Ebs': {
+                        'VolumeSize': volume['size_gb'],
+                        'DeleteOnTermination': True
+                        }
+                    })
+
+
+# Create the instance using stored config
+try:
+    instance = ec2.create_instances(
+        ImageId=config['server']['ami_type'],
+        MinCount=config['server']['min_count'],
+        MaxCount=config['server']['max_count'],
+        InstanceType=config['server']['instance_type'],
+        KeyName='ec2-keypair',
+        BlockDeviceMappings=volumes,
+        SubnetId=config['server']['security_group'],
+        # PrivateIpAddress='10.255.6.16',
+        SecurityGroupIds=config['server']['subnet'],
+        DryRun=True
+    )
+    iid = instance[0].id
+    print(f'Instance {iid} successfully created.')
+    print(f'IP Address: {instance.private_ip_address}')
+except Exception as e:
+    print('Failed to create instance:\n' + str(e))
+
--- create_keypair.py
+++ create_keypair.py
@@ -2,7 +2,7 @@ import boto3
 ec2 = boto3.resource('ec2')
 
 # create a file to store the key locally
-outfile = open('ec2-keypair.pem','w')
+outfile = open('ec2-keypair.pem', 'w')
 
 # call the boto ec2 function to create a key pair
 key_pair = ec2.create_key_pair(KeyName='ec2-keypair')
