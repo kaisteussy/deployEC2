cdf26c51d409e76410db68f4e68232994918cb7b
ksteussy <kai.steussy@gmail.com>
2021-05-10 08:58:12 -0500

    Moved key_pair and private_ip_address to config.yml.

--- create_instance.py
+++ create_instance.py
@@ -1,15 +1,14 @@
 import boto3
 import yaml
-import paramiko
 
 volumes = []
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# Set the resource object to EC2
+ec2 = boto3.resource('ec2')
+
+# Open and store UserData bash script
 with open('userdata.sh', 'r') as userdata_file:
     userdata = userdata_file.read()
-
-# Set the resource object
-ec2 = boto3.resource('ec2')
 
 # Open the configuration file and store its contents
 with open('config.yml', 'r') as config_file:
@@ -32,12 +31,12 @@ try:
         MinCount=config['server']['min_count'],
         MaxCount=config['server']['max_count'],
         InstanceType=config['server']['instance_type'],
-        KeyName='ec2-keypair',
+        KeyName=config['server']['key_pair'],
         BlockDeviceMappings=volumes,
         SubnetId=config['server']['network']['subnet'],
-        PrivateIpAddress='10.255.6.194',
+        PrivateIpAddress=config['server']['network']['ip_address'],
         SecurityGroupIds=config['server']['network']['security_groups'],
-        UserData=userdata
+        UserData=userdata,
         #DryRun=True
     )
     iid = instance[0].id
@@ -46,17 +45,3 @@ try:
 except Exception as e:
     print('Failed to create instance:\n' + str(e))
 
-
-# Function to SSH into the EC2 instance
-def ssh_connect(ssh, ip_address):
-    privkey = paramiko.RSAKey.from_private_key_file('ec2-keypair.pem')
-    try:
-        print('SSH into the instance: {}'.format(ip_address))
-        ssh.connect(hostname=ip_address,
-                    username='ec2-user', pkey=privkey)
-        return True
-    except Exception as e:
-        print(str(e))
-
-
-#ssh_connect(ssh, instance[0].private_ip_address)
