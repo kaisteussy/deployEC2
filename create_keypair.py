8faae5d6fbc79ed9a8a5d6b6400047e57260bbe9
ksteussy <kai.steussy@gmail.com>
2021-05-10 22:24:36 -0500

    Renamed keypair to be consistent with cloud-config.

--- create_keypair.py
+++ create_keypair.py
@@ -2,9 +2,9 @@ import boto3
 ec2 = boto3.resource('ec2')
 
 # call the boto ec2 function to create a key pair
-key_pair = ec2.create_key_pair(KeyName='ec2-keypair')
+key_pair = ec2.create_key_pair(KeyName='ec2-keypair2')
 
 # Capture the key and store it in a file
-with open('ec2-keypair.pem', 'w') as outfile:
+with open('ec2-keypair2.pem', 'w') as outfile:
     KeyPairOut = str(key_pair.key_material)
     outfile.write(KeyPairOut)
